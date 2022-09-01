4100a015078b9c2f65517f6bddcaffe18e94479c
GeorgeValle <jorgeguillermovalle@gmail.com>
2022-09-01 01:58:41 -0300

    fix IIFE
    implemented IIFE #read and #write

--- Contenedor.js
+++ Contenedor.js
@@ -6,6 +6,23 @@ const addressJProduct='./products.txt';
 
 //create the new class Container
 class Contenedor{
+    //IIFE
+    #read= async()=>{
+        try{
+            let data= await fs.promises.readFile(addressJProduct,'utf-8');
+            return JSON.parse(data);
+            // return products;
+        }catch (err){console.error(err)}
+    }
+
+    
+
+    #write= async(products)=>{
+        try{
+            await fs.promises.writeFile(addressJProduct, JSON.stringify(products,null,2))
+        }catch (err){console.error(err)}
+    }
+
     save= async (product) => {
         //validations
         if(!product.title||!product.price||!product.thumbnail) return{status:"error", message: "missing product"}
@@ -13,18 +30,27 @@ class Contenedor{
     try{
         
         if(fs.existsSync(addressJProduct)){
-            
-            let data= await fs.promises.readFile(addressJProduct,'utf-8');
-            let  products = JSON.parse(data);
+            //esto era antes
+            // let data= await fs.promises.readFile(addressJProduct,'utf-8');
+            // let  products = JSON.parse(data); 
+            let products= await this.#read();
             let  id = products[products.length-1].id+1;
+            
+            //products.id= (products) => products.reduce((max, prod) => prod.id > max ? prod.id : max, 0) + 1;
             product.id=id;
             products.push(product);
-            await fs.promises.writeFile(addressJProduct, JSON.stringify(products,null,2))
-            return{status:"success", message: `Product ${id} Created`};
+
+            //esto era antes
+            //await fs.promises.writeFile(addressJProduct, JSON.stringify(products,null,2)) 
+
+            //ahora escribe en un funcion
+            this.#write(products);
+            return{status:"success", message: `Product  Created`};
 
         }else{
-            product.id=1
+            product.id=1;
             await fs.promises.writeFile(addressJProduct, JSON.stringify([product],null,2))
+            
             return{status:"success", message: "Product created"}
 
         }
@@ -39,8 +65,10 @@ class Contenedor{
         //Validation
         if (!id) return {status: "error", message: "Id required"}
         if (fs.existsSync(addressJProduct)) {
-            let data = await fs.promises.readFile(addressJProduct, 'utf-8')
-            let products = JSON.parse(data)
+            //antes
+            // let data = await fs.promises.readFile(addressJProduct, 'utf-8')
+            // let products = JSON.parse(data)
+            let products = await this.#read();
             let product = products.find(product => product.id === id)
             if (product) return {status: "succes", message: product}
             return {status: "error", message: "Product not found"}
@@ -51,8 +79,10 @@ class Contenedor{
 
     getAll= async () => {
         if (fs.existsSync(addressJProduct)) {
-            let data = await fs.promises.readFile(addressJProduct, 'utf-8')
-            let products = JSON.parse(data)
+            //asi estaba antes
+            // let data = await fs.promises.readFile(addressJProduct, 'utf-8')
+            // let products = JSON.parse(data)
+            let products= await this.#read();
             return {status: "success", message: products}
         } else {
             return {status: "error", message: err.message}
@@ -63,13 +93,18 @@ class Contenedor{
         //Validation
         if (!id) return {status: "error", message: "Id required"}
         if (fs.existsSync(addressJProduct)) {
-            let data = await fs.promises.readFile(addressJProduct, 'utf-8')
-            let products = JSON.parse(data)
+            //antes
+            // let data = await fs.promises.readFile(addressJProduct, 'utf-8')
+            // let products = JSON.parse(data)
+            let products = await this.#read();
             
             let newProducts = products.filter(product => product.id !== id)
+            //este fue un intento fallido de slice
             // let indexId= products.findIdex(products.id===id)
             // let newProducts = products.slice(indexId);
-            await fs.promises.writeFile(addressJProduct, JSON.stringify(newProducts, null, 2))
+            this.#write(newProducts);
+            //esto estaba antes.
+            //await fs.promises.writeFile(addressJProduct, JSON.stringify(newProducts, null, 2))
             return {status: "success", message: "Product deleted!"}
         } else {
             return {status: "error", message: err.message}
@@ -78,7 +113,8 @@ class Contenedor{
 
     deleteAll = async () => {
         if (fs.existsSync(addressJProduct)) {
-            await fs.promises.writeFile(addressJProduct, JSON.stringify(null,null,2))
+            await fs.promises.writeFile(addressJProduct, JSON.stringify([],null,2))
+            //intento falliudo de unLink
             //await fs.unLink(addressJProduct)
             
             return {status: "success", message: "Products deleted!"}
--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const Contenedor = require('./Contenedor.js');
 const contenedor = new Contenedor();
 
 let product={
-    title:"Cien años de Soledad",
-    price: 500,
-    thumbnail: "http://GabrielGarciaMarquez.com/images/"
+    title:"Don Quijote de la Mancha",
+    price: 1600,
+    thumbnail: "http://MiguelDeCervantes.com/images/",
 }
 
 contenedor.save(product).then(result => console.log(result));
 //contenedor.getById(2).then(result => console.log(result));
 //contenedor.getAll().then(result => console.log(result));
-//contenedor.deleteById(1).then(result => console.log(result));
-// contenedor.deleteAll().then(result => console.log(result));
+//contenedor.deleteById(2).then(result => console.log(result));
+//contenedor.deleteAll().then(result => console.log(result));
