91552048be965ad7aa827df246bfa0686e6e7566
GeorgeValle <jorgeguillermovalle@gmail.com>
2022-10-09 09:09:23 -0300

    fix errors in router products

--- src/routes/chat.router.js
+++ src/routes/chat.router.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const sqlite= require('../options/sqlite.config');
+const tbl_table = "chats";
+
 
 const Manager = require('../controllers/chat.manager')
-const manager = new Manager()
+const manager = new Manager(sqlite,tbl_table)
 
 router.get('/', (req, res) => {
     manager.findAll().then(result => res.send(result))
@@ -10,7 +13,19 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     if (!req.body.email || !req.body.message) return res.send({error: 'data is required'})
-    manager.create(req.body).then(result => res.send(result))
+    // create the new objet `Date`
+    const now = new Date();
+    const date = now.toLocaleString();
+    const {email, msg} =req.body;
+    const message = {
+        email,
+        msg,
+        date
+    }
+
+
+
+    manager.create(message).then(result => res.send(result))
 })
 
 module.exports = router
--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const {MySQLOptions} = require('../options/mysql.config')
+const mysql = require('../options/mysql.config')
 
 const Manager = require('../controllers/product.manager')
-const manager = new Manager(MySQLOptions)
+const manager = new Manager(mysql,products)
 
 router.get('/', (req, res) => {
     let result = manager.findAll()
@@ -18,23 +18,34 @@ router.get('/:id', (req, res) => {
 })
 
 router.post('/', (req, res) => {
+    try{
     if (!req.body.title || !req.body.price || !req.body.thumbnail) return res.send({error: 'data is required'})
     const product = ({title, price, thumbnail}=req.body);
     let result = manager.create(product)
     res.send(result)
+    }catch{(err=>console.log(err))}
+    finally{(()=>mysql.destroy())}
+
 })
 
 router.put('/:id', (req, res) => {
+    try{
     if (!req.body.title || !req.body.price || !req.body.thumbnail) return res.send({error: 'data is required'})
     const product = ({title, price, thumbnail}=req.body)
     let result = manager.update(req.params.id, product)
     if (!result) return res.send({error: 'product was not found'})
     res.send(result)
+}catch{(err=>console.log(err))}
+finally{(()=>mysql.destroy())}
 })
 
 router.delete('/:id', (req, res) => {
+    try{
     let result = manager.delete(req.params.id)
     res.send(result)
+}catch{(err=>console.log(err))}
+finally{(()=>mysql.destroy())}
+
 })
 
 module.exports = router
